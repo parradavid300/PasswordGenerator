c1420aa476df3eeb894c22dead900fa6b5ddd784
David Garcia <parradavid300@yahoo.com>
2022-09-30 19:13:07 -0500

    Cleaned it up a little bit

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
+// Arrays for letters, numbers, and special characters //
+var numbersList = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseList = ["A", "B",  "C", "D", "E", "F", "G", "H", "I", "J", "K", 'L', "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"];
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Generating the random integer
@@ -30,30 +37,26 @@ function generatePassword() {
     window.alert("Wait! Password Length must be between 8 and 128 characters!");
     return 
   }
-// Prompts for user
-  var userWantsNumbers = window.confirm("Would you like to include numbers in your password?");
-  var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?");
-  var userWantsUppercase = window.confirm("Would you like to include uppercase letters in your password?");
-  var userWantsSpecialCharacters = window.confirm("Would you like to include special characters in your password?");
-// Arrays for letters, numbers, and special characters //
-  var numbersList = ["0","1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var lowerCaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCaseList = ["A", "B",  "C", "D", "E", "F", "G", "H", "I", "J", "K", 'L', "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var symbolList = ["!", "@", "#", "$", "%", "^", "&", "*"];
+  // Prompts for user
+var userNumbers = confirm("Would you like to include numbers in your password?");
+var userLowercase = confirm("Would you like to include lowercase letters in your password?");
+var userUppercase = confirm("Would you like to include uppercase letters in your password?");
+var userSpecialCharacters = confirm("Would you like to include special characters in your password?");
+
 
 //options cart holds the users choices //
   var optionsCart = []
 
-  if (userWantsNumbers === true) {
+  if (userNumbers === true) {
     optionsCart.push(numbersList);
   } 
-  if (userWantsLowercase === true) {
+  if (userLowercase === true) {
     optionsCart.push(lowerCaseList);
   } 
-  if (userWantsUppercase === true) {
+  if (userUppercase === true) {
     optionsCart.push(upperCaseList);
   } 
-  if (userWantsSpecialCharacters === true) {
+  if (userSpecialCharacters === true) {
     optionsCart.push(symbolList);
   } 
 //prompt if nothing is selected // 
@@ -84,5 +87,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// Add event listener to listen for click of the button
 generateBtn.addEventListener("click", writePassword);
